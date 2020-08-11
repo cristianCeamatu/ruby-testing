b33bc570ada7ca3f0d3355241088751ad87da895
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-11 13:38:56 +0300

    initialize method with super

--- main.rb
+++ main.rb
@@ -1,29 +1,20 @@
 class Animal
-  def speak
-    'Animals don`t speak!'
-  end
+  attr_accessor :name
+
+  def initialize; end
 end
 
 class Dog < Animal
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
+  def initialize(name, color)
+    super
+    @color = color
   end
 
   def speak
-    super + " But #{@name} says Bark Bark"
+    "But #{@name} - eyes #{@color} says Bark Bark"
   end
 end
 
-class Cat < Animal
-  def speak
-    puts 'Miaouuu'
-  end
-end
-
-dog = Dog.new('Cristian')
-cat = Cat.new
+dog = Dog.new('Red', 'Cristian')
 
 puts dog.speak
-puts cat.speak
