425b487625c97fc441be8ef4ceb691897b4a6cae
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-11 13:47:46 +0300

    More Modules

--- main.rb
+++ main.rb
@@ -1,31 +1,23 @@
-module Walkable
-  def walk
-    "I'm walking."
+module Mammal
+  class Dog
+    def speak(sound)
+      p "#{sound}"
+    end
+  end
+
+  class Cat
+    def say_name(name)
+      p "#{name}"
+    end
+  end
+
+  def self.expo(num)
+    num ** 2
   end
 end
 
-module Swimmable
-  def swim
-    "I'm swimming."
-  end
-end
+dog = Mammal::Dog.new
 
-module Climbable
-  def climb
-    "I'm climbing."
-  end
-end
+dog.speak('woff woff')
 
-class Animal
-  include Walkable
-  include Climbable
-
-  def speak
-    "I'm an animal, and I speak!"
-  end
-end
-
-puts Animal.ancestors
-
-fido = Animal.new
-p fido.climb
+p Mammal::expo(8)
